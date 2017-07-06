7fb19f025d742f84f50a9b3d34723fe1ea24836c
Yoav Maman <yoavmmn@gmail.com>
2017-07-06 22:24:40 +0300

    added webhook auth and random chuck norris joke

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const fetch = require('node-fetch');
+
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  res.send('hi');
+  const joke = await fetch('https://api.chucknorris.io/jokes/random');
+  res.send(joke.body);
+});
+
+router.get('/webhook', (req, res) => {
+  const token = process.env.WEBHOOK_TOKEN || '123456';
+
+  if (req.query['hub.verify_token'] === token) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Invalid token');
+  }
 });
 
 module.exports = router;
