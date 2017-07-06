97e0ca1b6d6ebd1840f5358e5427c471186e5203
Yoav Maman <yoavmmn@gmail.com>
2017-07-06 22:09:25 +0300

    Added main router

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const router = require('./routes/router');
+
+app.use('/', router);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Running on http://127.0.0.1:${PORT}/`);
