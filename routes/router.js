ee0925cda9107a24cdb2849901e46f5617f1debb
Etgar Shmueli <etgar90@gmail.com>
2017-07-07 00:45:11 +0300

    Fixed some linter errors

--- routes/router.js
+++ routes/router.js
@@ -6,8 +6,8 @@ const router = new express.Router();
 
 router.get('/', (req, res) => {
   (async function () {
-    let stream = await fetch('https://api.chucknorris.io/jokes/random');
-    let joke = await stream.json();
+    const stream = await fetch('https://api.chucknorris.io/jokes/random');
+    const joke = await stream.json();
     res.send(joke.value);
   }());
 });
@@ -17,14 +17,13 @@ router.get('/webhook', (req, res) => {
 
   if (req.query['hub.verify_token'] === token) {
     return res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Invalid token');
   }
+  return res.send('Invalid token');
 });
 
 
 router.post('/webhook', (req, res) => {
-  var data = req.body;
+  const data = req.body;
 
   // Make sure this is a page subscription
   if (data.object === 'page') {
@@ -38,12 +37,12 @@ router.post('/webhook', (req, res) => {
       entry.messaging.forEach((event) => {
         if (event.message) {
           // Handle the message
-          let messageParser = new MessageParser(event);
+          const messageParser = new MessageParser(event);
           messageParser.handle();
 
         } else {
           // This event doesn't contain any message...
-          console.log("Webhook received unknown event: ", event);
+          console.log('Webhook received unknown event: ', event);
         }
       });
     });
