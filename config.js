3779bfa4d1d95b730d0f41ae732a69c01d5fb84d
Etgar Shmueli <etgar90@gmail.com>
2017-07-07 10:05:06 +0300

    Added connection to mongodb

--- config.js
+++ config.js
@@ -4,8 +4,11 @@ const WELCOME_MESSAGE = 'Welcome, please enter a link.';
 
 const DONT_UNDERSTAND_MESSAGE = 'I\'m so sorry... But I cannot understand you. Please try something else :)';
 
+const DB_CONNECTION = 'mongodb://login_user:100200300@ds151452.mlab.com:51452/investores';
+
 module.exports = {
   PAGE_ACCESS_TOKEN,
   WELCOME_MESSAGE,
   DONT_UNDERSTAND_MESSAGE,
+  DB_CONNECTION,
 };
