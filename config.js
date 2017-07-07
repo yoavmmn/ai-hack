05f66bfe001c1206b393e3198092819b315ec4cc
Etgar Shmueli <etgar90@gmail.com>
2017-07-07 03:59:11 +0300

    Some more logic

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const PAGE_ACCESS_TOKEN = 'EAAEc25tPWYEBAFcourIvkvObvZBLqJZAQ2b12IpaAIk2IxZAa1V94CbsoZAPHeulEZBkfBl1KrJDxTlseIG6iwSLEXQl8OZBpPOjNYP95fFKr7d74bQcUtHlMLOxD6qzrJITlry5xtWSqZCf4ZCat0DnIZAty2pVUKlP1M3ocqIakHQZDZD';
 
+const WELCOME_MESSAGE = 'Welcome, please enter a link.';
+
 module.exports = {
   PAGE_ACCESS_TOKEN,
+  WELCOME_MESSAGE,
 };
